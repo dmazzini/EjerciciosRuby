d10f01872d41705a391e491d0367c9c6578aeb2f
dmazzini <dmazzini@gmail.com>
2014-08-15 12:41:24 -0300

    primer commit

---
+++ EvaluadorDeEstrategias/EvaluadorEstrategias.rb
@@ -0,0 +1,49 @@
+require 'test/unit'
+ 
+class XXX < Test::Unit::TestCase
+  def test1
+	efectivoInicial = 1000000
+	agente = Agente.new(efectivoInicial)
+	
+	agente.comprar(100.5, "GGAL", 75)
+	
+    assert_equal(999899.5, agente.efectivo)
+	assert_equal(75, agente.cantidadAccionesDe("GGAL"))
+  end
+  
+    def test2
+	efectivoInicial = 1000000
+	agente = Agente.new(efectivoInicial)
+	
+	agente.comprar(100.5, "GGAL", 75)
+	agente.vender(70.3, "GGAL", 22)
+	
+    assert_equal(999969.8, agente.efectivo)
+	assert_equal(53, agente.cantidadAccionesDe("GGAL"))
+  end
+end
+
+class Agente
+	def initialize(efectivo)
+		@efectivo = efectivo
+		@acciones = Hash.new(0)
+	end
+	
+	def efectivo
+		@efectivo
+	end
+	
+	def comprar(monto, nombreEmpresa, cantidadAcciones)
+		@efectivo -= monto
+		@acciones[nombreEmpresa] += cantidadAcciones
+	end
+	
+	def vender(monto, nombreEmpresa, cantidadAcciones)
+		@efectivo += monto
+		@acciones[nombreEmpresa] -= cantidadAcciones
+	end
+	
+	def cantidadAccionesDe(nombreEmpresa)
+		@acciones[nombreEmpresa]
+	end
+end

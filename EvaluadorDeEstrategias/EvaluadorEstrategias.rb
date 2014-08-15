0ce85314694b96a7717e61ee935c764a6ce3697f
dmazzini <dmazzini@gmail.com>
2014-08-15 15:09:52 -0300

    Se agregan las fechas

--- EvaluadorDeEstrategias/EvaluadorEstrategias.rb
+++ EvaluadorDeEstrategias/EvaluadorEstrategias.rb
@@ -1,11 +1,12 @@
 require 'test/unit'
+require 'date'
  
 class XXX < Test::Unit::TestCase
   def testComprar
 	efectivoInicial = 1000000
 	agente = Agente.new(efectivoInicial)
 	
-	agente.comprar(100.5, "GGAL", 75)
+	agente.comprar(100.5, "GGAL", 75, Date.new(2014, 3, 1))
 	
     assert_equal(999899.5, agente.efectivo)
 	assert_equal(75, agente.cantidadAccionesDe("GGAL"))
@@ -15,8 +16,8 @@ class XXX < Test::Unit::TestCase
 	efectivoInicial = 1000000
 	agente = Agente.new(efectivoInicial)
 	
-	agente.comprar(100.5, "GGAL", 75)
-	agente.vender(70.3, "GGAL", 22)
+	agente.comprar(100.5, "GGAL", 75, Date.new(2014, 3, 1))
+	agente.vender(70.3, "GGAL", 22, Date.new(2014, 3, 2))
 	
     assert_equal(999969.8, agente.efectivo)
 	assert_equal(53, agente.cantidadAccionesDe("GGAL"))
@@ -33,12 +34,12 @@ class Agente
 		@efectivo
 	end
 	
-	def comprar(monto, nombreEmpresa, cantidadAcciones)
+	def comprar(monto, nombreEmpresa, cantidadAcciones, fecha)
 		@efectivo -= monto
 		@acciones[nombreEmpresa] += cantidadAcciones
 	end
 	
-	def vender(monto, nombreEmpresa, cantidadAcciones)
+	def vender(monto, nombreEmpresa, cantidadAcciones, fecha)
 		@efectivo += monto
 		@acciones[nombreEmpresa] -= cantidadAcciones
 	end

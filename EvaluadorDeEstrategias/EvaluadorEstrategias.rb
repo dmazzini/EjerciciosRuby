1370807189bcfa70af98a377533160e2bdeb9751
Damián Mazzini <dmazzini@gmail.com>
2014-08-17 20:06:37 -0300

    Se modifica accion

--- EvaluadorDeEstrategias/EvaluadorEstrategias.rb
+++ EvaluadorDeEstrategias/EvaluadorEstrategias.rb
@@ -4,32 +4,32 @@ require 'set'
 
 class XXX < Test::Unit::TestCase
   def testComprar
-	efectivoInicial = 1000000
-	agente = Agente.new(efectivoInicial)
-	
-	agente.comprar(100.5, "GGAL", 75, Date.new(2014, 3, 1))
-	
-    assert_equal(999899.5, agente.efectivo)
-	assert_equal(75, agente.cantidadAccionesDe("GGAL"))
+    efectivoInicial = 1000000
+    agente = Agente.new(efectivoInicial)
+
+    agente.comprar("GGAL", 75, 13, Date.new(2014, 3, 1))
+
+    assert_equal(999025, agente.efectivo)
+    assert_equal(75, agente.cantidadAccionesDe("GGAL"))
   end
-  
-    def testVender
-	 efectivoInicial = 1000000
-	 agente = Agente.new(efectivoInicial)
-	
-	 agente.comprar(100.5, "GGAL", 75, Date.new(2014, 3, 1))
-	 agente.vender(70.3, "GGAL", 22, Date.new(2014, 3, 2))
-	
-    assert_equal(999969.8, agente.efectivo)
-  	assert_equal(53, agente.cantidadAccionesDe("GGAL"))
+
+  def testVender
+    efectivoInicial = 1000000
+    agente = Agente.new(efectivoInicial)
+
+    agente.comprar("GGAL", 75, 13, Date.new(2014, 3, 1))
+    agente.vender("GGAL", 12)
+
+    assert_equal(999925, agente.efectivo)
+    assert_equal(0, agente.cantidadAccionesDe("GGAL"))
   end
 end
 
 class Accion
-  def initialize(nombreEmpresa, monto, cantidad, fecha)
+  def initialize(nombreEmpresa, cantidad, cotizacion, fecha)
     @nombreEmpresa = nombreEmpresa
-    @monto = monto
     @cantidad = cantidad
+    @cotizacion = cotizacion
     @fecha = fecha
   end
 
@@ -43,26 +43,35 @@ class Accion
 end
 
 class Agente
-	def initialize(efectivo)
-		@efectivo = efectivo
-		@acciones = Set.new
-	end
-	
-	def efectivo
-		@efectivo
-	end
-	
-	def comprar(monto, nombreEmpresa, cantidadAcciones, fecha)
-		@efectivo -= monto
-		@acciones.add(Accion.new(nombreEmpresa, monto, cantidadAcciones, fecha))
-	end
-	
-	def vender(monto, nombreEmpresa, cantidadAcciones, fecha)
-		@efectivo += monto
-		@acciones.delete_if {|accion| accion.nombreEmpresa == nombreEmpresa}
-	end
-	
-	def cantidadAccionesDe(nombreEmpresa)
-		(@acciones.keep_if {|accion| accion.nombreEmpresa == nombreEmpresa}).inject { |sum, accion| sum + accion.cantidad }
-	end
+  def initialize(efectivo)
+    @efectivo = efectivo
+    @acciones = Set.new
+  end
+
+  def efectivo
+    @efectivo
+  end
+
+  def comprar(nombreEmpresa, cantidad, cotizacion, fecha)
+    @efectivo -= (cantidad*cotizacion)
+    @acciones.add(Accion.new(nombreEmpresa, cantidad, cotizacion, fecha))
+  end
+
+  def vender(nombreEmpresa, cotizacion)
+    @efectivo += (cantidadAccionesDe(nombreEmpresa)*cotizacion)
+    @acciones.delete_if { |accion| accion.nombreEmpresa == nombreEmpresa }
+  end
+
+  def cantidadAccionesDe(nombreEmpresa)
+    #Ver como hacer con inject
+    #accionesDeEmpresa = (@acciones.keep_if {|accion| accion.nombreEmpresa == nombreEmpresa})
+    #accionesDeEmpresa.inject { |sum, accion| sum + accion.cantidad }
+    sum = 0
+    @acciones.each do |accion|
+      if accion.nombreEmpresa == nombreEmpresa
+        sum += accion.cantidad
+      end
+    end
+    sum
+  end
 end
